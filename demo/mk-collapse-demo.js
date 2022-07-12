e3aa810907a4a4f821c72bede33ab63f6f52e97a
Hiten Bhayani <hitenbhayani41@gmail.com>
2022-07-13 03:05:32 +0530

    update demo page

--- demo/mk-collapse-demo.js
+++ demo/mk-collapse-demo.js
@@ -5,8 +5,8 @@ export class MkCollapseDemo extends LitElement {
   render() {
     return html` <button @click=${this._onToggle}>Toggle</button>
       <mk-collapse open
-        ><span
-          >Lorem Ipsum is simply dummy text of the printing and typesetting
+        ><div>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
@@ -14,8 +14,8 @@ export class MkCollapseDemo extends LitElement {
           remaining essentially unchanged. It was popularised in the 1960s with
           the release of Letraset sheets containing Lorem Ipsum passages, and
           more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.</span
-        ></mk-collapse
+          including versions of Lorem Ipsum.
+        </div></mk-collapse
       >`;
   }
 
